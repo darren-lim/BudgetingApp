f3b6848f66b4d2fa5da16051ed9ff269af7835fa
Timothy <timfsim@gmail.com>
2020-05-05 16:02:29 -0700

    Added the textbox thingy to the date and source

--- budgeting/forms.py
+++ budgeting/forms.py
@@ -3,18 +3,33 @@ from .fields import ListTextWidget
 from .models import Transaction
 
 
-class FormForm(forms.ModelForm):
+class TransactionForm(forms.ModelForm):
 
     class Meta:
         model = Transaction
-        fields = ['amount', 'source', 'notes']
+        fields = ['amount', 'source', 'date_posted', 'notes']
+        widgets = {
+            'date_posted': forms.TextInput(attrs={'placeholder': 'mm/dd/yyyy'}),
+            'source': forms.TextInput(attrs={'placeholder': 'e.g. Part-time job, My Bank Account'})
+        }
 
     def __init__(self, *args, **kwargs):
         _source_list = kwargs.pop('source', None)
         user = kwargs.pop('user', None)
-        super(FormForm, self).__init__(*args, **kwargs)
+        super(TransactionForm, self).__init__(*args, **kwargs)
 
         # the "name" parameter will allow you to use the same widget more than once in the same
-            # form, not setting this parameter differently will cuse all inputs display the
-            # same list.
-        self.fields['source'].widget = ListTextWidget(data_list=_source_list, name='source')
+        # form, not setting this parameter differently will cuse all inputs display the
+        # same list.
+        self.fields['source'].widget = ListTextWidget(
+            data_list=_source_list, name='source')
+
+
+class UpdateForm(TransactionForm):
+    class Meta:
+        model = Transaction
+        fields = ['t_type', 'amount', 'source', 'date_posted', 'notes']
+        widgets = {
+            'date_posted': forms.TextInput(attrs={'placeholder': 'mm/dd/yyyy'}),
+            'source': forms.TextInput(attrs={'placeholder': 'e.g. Part-time job, My Bank Account'})
+        }
--- budgeting/views.py
+++ budgeting/views.py
@@ -4,10 +4,8 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView, DetailView, CreateView, UpdateView, DeleteView)
-from django.http import HttpResponse, HttpResponseRedirect
 from .models import Transaction
-
-from .forms import FormForm
+from .forms import TransactionForm, UpdateForm
 
 
 class TransListView(ListView):
@@ -17,7 +15,7 @@ class TransListView(ListView):
     # the "-" sign makes transactions order from newest to oldest (top-bottom order)
     ordering = ['-date_posted']
 
-    #maybe get the specific models???
+    # maybe get the specific models???
 
     def get_queryset(self):
         if self.request.user.is_authenticated:
@@ -33,11 +31,8 @@ class TransDetailView(DetailView):
 
 
 class TransCreateView(LoginRequiredMixin, CreateView):
-    #source = ('Job', 'Food', 'Gas')
     model = Transaction
-    form_class = FormForm
-    # model = Transaction
-
+    form_class = TransactionForm
     template_name = 'budgeting/transaction_form.html'
 
     def get_context_data(self, **kwargs):
@@ -64,9 +59,7 @@ class TransCreateView(LoginRequiredMixin, CreateView):
 # UserPassesTestMixin can be used as a parameter if we implement test_func
 class TransUpdateView(LoginRequiredMixin, UpdateView):
     model = Transaction
-    form_class = FormForm
-    # model = Transaction
-
+    form_class = UpdateForm
     template_name = 'budgeting/transaction_update.html'
 
     # if I leave out get_form() the object is successfully saved
@@ -82,60 +75,25 @@ class TransUpdateView(LoginRequiredMixin, UpdateView):
     def form_valid(self, form):  # sets the logged in user as the author of that transaction
         form.instance.author = self.request.user
         return super().form_valid(form)
-    '''
-    model = Transaction
-    fields = ['amount', 'source', 'notes']
-
-    def form_valid(self, form):  # sets the logged in user as the author of that transaction
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-    
-    def test_func(self): # prevents any other users from updating but this shouldn't happen in the first place because transactions are private (more for smth like twitter)
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False'''
 
 
 class TransDeleteView(LoginRequiredMixin, DeleteView):
     model = Transaction
     success_url = '/'  # goes to the homepage with all transactions.
+
     '''def test_func(self): # prevents any other users from updating but this shouldn't happen in the first place because transactions are private (more for smth like twitter)
         post = self.get_object()
         if self.request.user == post.author:
             return True
         return False'''
 
-def country_form(request, parameter):
-    # instead of hardcoding a list you could make a query of a model, as long as
-    # it has a __str__() method you should be able to display it.
-    country_list = ('Mexico', 'USA', 'China', 'France')
-    form = FormForm(data_list=country_list)
-
-    return render(request, 'budgeting/transaction_form.html', {
-        'form': form
-    })
 
 def about(request):
     # in the 3rd paramter we pass in the title directly as a dictionary. This will pass into our about.html.
     return render(request, 'budgeting/about.html', {'title': 'About'})
 
 
-'''transactions = [
-    {'type': 'Withdrawal',
-     'amount': '70',
-     'source': 'Groceries',
-     'notes': '',
-     'date_posted': 'April 4, 2020'},
-
-    {'type': 'Deposit',
-     'amount': '20',
-     'source': 'Part-time Job',
-     'notes': '',
-     'date_posted': 'April 4, 2020'}
-]
-
-
+'''
 def home(request):
     context = {'transactions': Transaction.objects.all()}
     # reference subdirectory within the template file.
