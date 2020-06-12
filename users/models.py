01cd2869e89f811745ee9b5b9d4ccd9930c94a15
Andrew Kwon <andrewyulkwon@gmail.com>
2020-06-11 21:41:55 -0700

    Added React and Register/Login/Logout

--- users/models.py
+++ users/models.py
@@ -6,9 +6,6 @@ from PIL import Image
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return '{} Profile'.format(self.user.username)
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
