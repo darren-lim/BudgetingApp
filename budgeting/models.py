19ecff1539870789c71d4af4644a01fdd147f976
Darren <dartrenbbx@gmail.com>
2020-07-14 01:52:20 -0700

    models remove djongo

--- budgeting/models.py
+++ budgeting/models.py
@@ -1,6 +1,7 @@
 import decimal
 
-from djongo import models  # databases!
+#from djongo import models  # databases!
+from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
